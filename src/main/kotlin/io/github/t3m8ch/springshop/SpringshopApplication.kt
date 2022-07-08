1c61b5e385bdb1e753e49e08202bb3334c3fc422
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-08 11:12:38 +0400

    feat: Init project

---
+++ src/main/kotlin/io/github/t3m8ch/springshop/SpringshopApplication.kt
@@ -0,0 +1,11 @@
+package io.github.t3m8ch.springshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringshopApplication>(*args)
+}
