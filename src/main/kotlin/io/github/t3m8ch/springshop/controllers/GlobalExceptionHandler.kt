66819a91fd13b3b41b2ad69fcf75f145658f2551
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-08 23:20:14 +0400

    feat: Create GlobalExceptionHandler

---
+++ src/main/kotlin/io/github/t3m8ch/springshop/controllers/GlobalExceptionHandler.kt
@@ -0,0 +1,15 @@
+package io.github.t3m8ch.springshop.controllers
+
+import io.github.t3m8ch.springshop.exceptions.APIErrorBody
+import io.github.t3m8ch.springshop.exceptions.BaseAPIException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(BaseAPIException::class)
+    fun handleBaseAPIException(ex: BaseAPIException): ResponseEntity<APIErrorBody> {
+        return ResponseEntity(ex.body, ex.httpStatus)
+    }
+}
