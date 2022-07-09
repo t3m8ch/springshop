8a30d3cc413e350ddae0e35e70da87ee3a22cc93
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-09 18:16:06 +0400

    feat: Create ProductController

---
+++ src/main/kotlin/io/github/t3m8ch/springshop/services/interfaces/ProductServiceImpl.kt
@@ -0,0 +1,37 @@
+package io.github.t3m8ch.springshop.services.interfaces
+
+import io.github.t3m8ch.springshop.dto.CreateUpdateProductDTO
+import io.github.t3m8ch.springshop.dto.ProductOutDTO
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class ProductServiceImpl : ProductService {
+    override fun getAll(categoryId: UUID?): List<ProductOutDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getById(id: UUID): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun create(dto: CreateUpdateProductDTO): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateById(id: UUID, dto: CreateUpdateProductDTO): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeById(id: UUID): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: UUID): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun restoreById(id: UUID): ProductOutDTO {
+        TODO("Not yet implemented")
+    }
+}
